b3ce74a9e58e1509edd2f2a7a9266d6ec1f3dc5d
Bailog <bailogru@gmail.com>
2016-05-14 00:02:50 +0700

    improved code

--- BCG.js
+++ BCG.js
@@ -1,34 +1,54 @@
 //=============================================================================
 // Bailog Character Generator
 // BCG.js
-// Version: 0.1
+// Version: 0.2
 //=============================================================================
 
 //=============================================================================
  /*:
- * @plugindesc CharacterGenerator
+ * @plugindesc Алгоритм рандомной генерации персонажей. Включает в себя
+ * создание класса, пола, имени, выбор графики и генерацию пассивных умений.
+ * @help
+ * Классы:
+ * 1 - Рыцарь
+ * 6 - Плут
+ * 11 - Архимаг
+ * 16 - Клирик
+ * Имена:
+ * Мужские: Массив MaleName
+ * Женские: Массив FemaleName
  * @author Bailog
  */
 //=============================================================================
 
-var MaleName = ['Ридверг','Регад','Когрин','Мантон','Барклай','Торвальд'];
+var MaleName = ['Ридверг','Регард','Когрин','Мантон','Барклай','Торвальд'];
 var FemaleName = ['Марга','Тарна','Аделин','Мия','Нора','Райна'];
 
-function randomInteger(min, max) {
+function randomInteger(min, max){
     var rand = min + Math.random() * (max + 1 - min);
     rand = Math.floor(rand);
     return rand;
 }
 
+function generateClass(act, skill, wep, arm){
+	$gameActors.actor(act).learnSkill(skill);
+	$gameParty.gainItem($dataWeapons[wep], 1);
+	$gameParty.gainItem($dataArmors[arm], 1);
+	$gameActors.actor(act).changeEquip(0, $dataWeapons[wep]);
+	$gameActors.actor(act).changeEquip(3, $dataArmors[arm]);
+	$gameActors.actor(act).gainMp(1000)
+	$gameActors.actor(act).gainHp(1000)
+}
+
 function generateCharacters() {
 for (var i = 1; i <= $gameVariables.value(1); i++){
 	$gameActors.actor(i).clearProfileStatusText();
-	for(var k = 3 ; k<=20; k++) $gameActors.actor(i).forgetSkill(k);
-
+	for(var k = 3 ; k<=40; k++) $gameActors.actor(i).forgetSkill(k);
 	var role = i;
 	var charClass;
 	var roll;
 	
+	// === Выбор класса ===
 	if (role == 1) {
 		charClass = 1;
 		$gameActors.actor(i).changeClass(charClass, false);
@@ -51,34 +71,20 @@ for (var i = 1; i <= $gameVariables.value(1); i++){
 		
 		$gameActors.actor(i).changeClass(charClass, false);
 	}
+	// ======================
 	
-	if ($gameActors.actor(i).currentClass().id == 1){
-		$gameActors.actor(i).learnSkill(4);
-		$gameParty.gainItem($dataWeapons[1], 1);
-		$gameActors.actor(i).changeEquip(0, $dataWeapons[1]);
-	} 
-	if ($gameActors.actor(i).currentClass().id == 6){
-		$gameActors.actor(i).learnSkill(11);
-		$gameParty.gainItem($dataWeapons[2], 1);
-		$gameActors.actor(i).changeEquip(0, $dataWeapons[2]);
-	} 
-	if ($gameActors.actor(i).currentClass().id == 11){
-		$gameActors.actor(i).learnSkill(9);
-		$gameParty.gainItem($dataWeapons[3], 1);
-		$gameActors.actor(i).changeEquip(0, $dataWeapons[3]);
-	} 
-	if ($gameActors.actor(i).currentClass().id == 16){
-		$gameActors.actor(i).learnSkill(12);
-		$gameParty.gainItem($dataWeapons[3], 1);
-		$gameActors.actor(i).changeEquip(0, $dataWeapons[3]);
-	} 
+	// === Создание стартовых скиллов и шмота для класса ===
+	if ($gameActors.actor(i).currentClass().id == 1) generateClass(i, 10, 1, 1);
+	if ($gameActors.actor(i).currentClass().id == 6) generateClass(i, 20, 2, 2);
+	if ($gameActors.actor(i).currentClass().id == 11) generateClass(i, 30, 3, 3);
+	if ($gameActors.actor(i).currentClass().id == 16) generateClass(i, 40, 3, 3);
+	// =====================================================
 	
-	
+	// === Создание пола персонажа, его имя, и спрайты ===
 	roll = Math.floor(Math.random()*2);
 	if (roll == 0){ 
 		roll = Math.floor(Math.random()*6);
 		$gameActors.actor(i).setName(MaleName[roll]);
-		
 		roll = randomInteger(0,0);
 		$gameActors.actor(i).setFaceImage(String(charClass), roll);
 		$gameActors.actor(i).setCharacterImage(String(charClass), roll);
@@ -88,13 +94,14 @@ for (var i = 1; i <= $gameVariables.value(1); i++){
 	if (roll == 1){
 		roll = Math.floor(Math.random()*6); 
 		$gameActors.actor(i).setName(FemaleName[roll]);
-		
 		roll = randomInteger(4,4);
 		$gameActors.actor(i).setFaceImage(String(charClass), roll);
 		$gameActors.actor(i).setCharacterImage(String(charClass), roll);
 		$gameActors.actor(i).setBattlerImage(String(charClass)+ '_' + String(roll));
 	}
+	// =========================================
 	
+	// === Генератор пассивок ===
 	var traitArr = Array(4);
 	var min = 22;
 	var max = 28;
@@ -113,10 +120,11 @@ for (var i = 1; i <= $gameVariables.value(1); i++){
 	
 	for (var j = 0; j < 3; j++){
 		if (traitArr[j] == 22){
-			$gameActors.actor(i).addProfileStatusText('passivki');
+			$gameActors.actor(i).addProfileStatusText('passivki indev');
 			
 		}
 	}
+	// =========================================
 }
-
+	
 };
